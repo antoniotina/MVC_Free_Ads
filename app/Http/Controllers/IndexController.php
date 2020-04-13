87afdb93c9bb31d809a82f6a909297d1b583c9e2
unknown <antonioarantestina@gmail.com>
2020-04-13 16:58:21 +0200

    First steps with Laravel. Login/Registration with email verification

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function showIndex()
+    {
+        return view('index');
+    }
+
+    public function index()
+    {
+        return view('index');
+    }
+}
